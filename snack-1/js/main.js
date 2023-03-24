2108013a669482f9b13b3e09b6f351cfbd0d351a
DanieleMorello <danielemorello@live.it>
2023-03-24 12:16:13 +0100

    Structure with text of exercise and the array zucchine

---
+++ snack-1/js/main.js
@@ -0,0 +1,58 @@
+/*
+  Crea un array di 10 oggetti che rappresentano una zucchina, 
+  indicando per ognuna varietà, peso e lunghezza. 
+  Calcola quanto pesano tutte le zucchine.
+*/
+
+const zucchine = [
+  {
+    varieta: 'varieta1',
+    peso: 1,
+    lunghezza: 1
+  },
+  {
+    varieta: 'varieta2',
+    peso: 2,
+    lunghezza: 2
+  },
+  {
+    varieta: 'varieta3',
+    peso: 3,
+    lunghezza: 3
+  },
+  {
+    varieta: 'varieta4',
+    peso: 4,
+    lunghezza: 4
+  },
+  {
+    varieta: 'varieta5',
+    peso: 5,
+    lunghezza: 5
+  },
+  {
+    varieta: 'varieta6',
+    peso: 6,
+    lunghezza: 6
+  },
+  {
+    varieta: 'varieta7',
+    peso: 7,
+    lunghezza: 7
+  },
+  {
+    varieta: 'varieta8',
+    peso: 8,
+    lunghezza: 8
+  },
+  {
+    varieta: 'varieta9',
+    peso: 9,
+    lunghezza: 9
+  },
+  {
+    varieta: 'varieta10',
+    peso: 10,
+    lunghezza: 10
+  }
+]
